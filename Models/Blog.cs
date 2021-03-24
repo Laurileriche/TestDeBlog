982a8ba6473fa94ceedd087bad65e912c09cdceb
Aurele ASSOGBA <aureleassogba@gmail.com>
2021-03-23 23:02:10 -0400

    Add project files.

---
+++ Models/Blog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestDeBlog.Models
+{
+    public class Blog
+    {
+        [Key]
+        public int BlogID { get; set; }
+
+        [MaxLength(100)]
+        [Required(ErrorMessage = "Enter your name please")]
+        [Display(Name = "Name")]
+        public string Nom { get; set; }
+
+      
+        [Required(ErrorMessage = "Enter your content please")]
+        [Display(Name = "Content")]
+        public string Content { get; set; }
+
+
+        [Display(Name = "Date Crée: ")]
+        public DateTime DateFacturation { get; set; }
+
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,4 @@
+﻿//var colors = ['#E3DFFD', '#DBF9FA', '#ECDBFA', '#F8DBFA', '#DBDEFA', '#DBFAE6', '#FAF0DB'];
+//var r_color = colors[Math.floor(Math.random() * colors.length)];
+
+//$('#titre').css('background-color', r_color);
